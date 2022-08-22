4c282f3e8e0091f74c851b069cf3ec114ee2996e
Sharmane <sjmburlaza@gmail.com>
2022-08-22 22:18:28 +0800

    DB connection successful

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const cors = require('cors');
+
+dotenv.config();
+app.use(cors());
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DBconnection is successful!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`API is now online on port ${ process.env.PORT || 4000 }`)
+});
